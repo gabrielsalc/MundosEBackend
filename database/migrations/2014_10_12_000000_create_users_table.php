cbb96ca47b0d12c008b4bc8ae9ce92042164ebdb
gabrielsalc <gabrielsalcedo.gs@gmail.com>
2022-06-04 14:53:09 -0300

    agregadas migraciones

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,9 +14,9 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->id(); //integer unsigned increment
+            $table->string('name'); //varchar(250) se le puede colocar especificamente
+            $table->string('email')->unique(); //email unique
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
