94bdcacbf97fdf25b4a3ab4ac42915393efcddd2
gabrielsalc <gabrielsalcedo.gs@gmail.com>
2022-06-06 20:47:47 -0300

    agregado base de datos

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -23,18 +23,8 @@
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-                <ul class="navbar-nav  mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link" href="/">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/productos">Productos</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/quienessomos">Quienes Somos</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/contact">Contacto</a>
+                        <a class="nav-link" href="/productos">MENSAJES</a>
                     </li>
                 </ul>
             </div>
--- resources/views/productos/index.blade.php
+++ resources/views/productos/index.blade.php
@@ -2,8 +2,8 @@
 @section('title', 'Home del Sitio')
 @section('contenido')
 <div class="col-12 col-md-8 mx-auto my-5">
-    <h1>Productos</h1>
-    <p>Todos Nuestros Productos</p>
+    <h1>MENSAJES</h1>
+    <p>TODOS LOS MENSAJES</p>
     <a class="btn btn-primary mb-4" href="/productos/create">Crear Producto</a>
     <div class="row">
         @if (session('status'))
@@ -16,16 +16,11 @@
             <div class="card">
                 <div class="card-body">
                     <h5 class="card-title"><a href="/productos/{{$producto->id}}">{{$producto->name}}</a></h5>
-                    <h3 class="card-text">${{$producto->email}}</h3>
-                    <h3 class="card-text">${{$producto->precio}}</h3>
-                    <p class="card-text">{{$producto->descripcion}}</p>
+                    <h3 class="card-text">{{$producto->email}}</h3>
+                    <h3 class="card-text">{{$producto->precio}}</h3>
+                    <h3 class="card-text">{{$producto->descripcion}}</h3>
                 </div>
                 <div class="card-footer d-flex justify-content-around">
-                    <a href="/productos/edit/{{$producto->id}}" class="btn btn-secondary">Editar</a>
-                    <form action="{{route('productos.destroy',$producto)}}" method="POST">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit" class="btn btn-danger">Borrar</button>
                     </form>
                 </div>
             </div>
