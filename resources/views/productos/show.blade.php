c2ed907de1aae5393600f68e07131bafa9780142
gabrielsalc <gabrielsalcedo.gs@gmail.com>
2022-06-06 21:03:56 -0300

    agregado base de datos

--- resources/views/productos/show.blade.php
+++ resources/views/productos/show.blade.php
@@ -1,7 +1,8 @@
 @extends('layout')
 @section('contenido')
 <div class="col-12 mx-auto my-5"> 
-    <h1>Producto {{$producto->name}}</h1>
+    <h1>Mensaje </h1>
+    <p>{{$producto->name}}</p>
     <p>{{$producto->email}}</p>
     <p>{{$producto->precio}}</p>
     <p>{{$producto->descripcion}}</p>  
